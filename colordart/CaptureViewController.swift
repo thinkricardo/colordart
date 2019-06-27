ebbbb7b3c687a63c18b84e81bc40c7d53b6d0585
Ricardo Pereira <ricardo.mlpp@gmail.com>
2019-06-27 22:20:29 +0100

    feat(capture): add capture view controller

---
+++ colordart/CaptureViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CaptureViewController.swift
+//  colordart
+//
+//  Created by Ricardo Pereira on 27/06/2019.
+//  Copyright © 2019 octoloop. All rights reserved.
+//
+
+import Cocoa
+
+class CaptureViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
