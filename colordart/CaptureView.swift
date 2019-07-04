037a6f0304b7f4f000a00acfbb025aa1c2183f04
Ricardo Pereira <ricardo.mlpp@gmail.com>
2019-07-04 22:24:08 +0100

    feat(capture): add capture view

---
+++ colordart/CaptureView.swift
@@ -0,0 +1,19 @@
+//
+//  CaptureView.swift
+//  colordart
+//
+//  Created by Ricardo Pereira on 04/07/2019.
+//  Copyright © 2019 octoloop. All rights reserved.
+//
+
+import Cocoa
+
+class CaptureView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
