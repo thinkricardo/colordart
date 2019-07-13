927691777972b30582363792ecdfdd2eebbb3daa
Ricardo Pereira <ricardo.mlpp@gmail.com>
2019-07-13 19:24:41 +0100

    feat(info): add info view

---
+++ colordart/Capture/CaptureInfoView.swift
@@ -0,0 +1,19 @@
+//
+//  CaptureInfoView.swift
+//  colordart
+//
+//  Created by Ricardo Pereira on 13/07/2019.
+//  Copyright © 2019 octoloop. All rights reserved.
+//
+
+import Cocoa
+
+class CaptureInfoView: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
